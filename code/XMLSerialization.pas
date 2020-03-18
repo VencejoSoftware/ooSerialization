f082f2720864c57f5f2addfaa40319765bac99da
Vencejo Software <vencejosoftware@gmail.com>
2020-03-18 13:37:35 -0300

    + Added new XML list serialization node resolve methods

--- code/XMLSerialization.pas
+++ code/XMLSerialization.pas
@@ -38,13 +38,22 @@ type
   end;
 
   TXMLListSerialize<T> = class(TInterfacedObject, IListSerialize<T>)
+  type
+    TFindNodeCallback = reference to function(const XMLNode: IXMLNode; const RootNode: WideString): IXMLNodeList;
+    TRootNodeCallback = reference to function: WideString;
   strict private
+    _FindNodeCallback: TFindNodeCallback;
+    _RootNodeCallback: TRootNodeCallback;
     _ItemSerialize: IItemSerialize<T>;
+  private
+    function AddTagDelimiter(const TagName: WideString; const IsClosed: Boolean): WideString;
   public
     function Decompose(const List: IIterableList<T>): WideString;
     function Compose(const Text: WideString; const List: IIterableList<T>): Boolean;
-    constructor Create(const ItemSerialize: IItemSerialize<T>);
-    class function New(const ItemSerialize: IItemSerialize<T>): IListSerialize<T>;
+    constructor Create(const ItemSerialize: IItemSerialize<T>; const FindNodeCallback: TFindNodeCallback;
+      const RootNodeCallback: TRootNodeCallback);
+    class function New(const ItemSerialize: IItemSerialize<T>; const FindNodeCallback: TFindNodeCallback = nil;
+      const RootNodeCallback: TRootNodeCallback = nil): IListSerialize<T>;
   end;
 
   TXMLSerialization<T> = class(TInterfacedObject, ISerialization<T>)
@@ -52,7 +61,8 @@ type
     _ItemSerialize: IItemSerialize<T>;
     _ListSerialize: IListSerialize<T>;
   protected
-    function FindNodeCallback(const XMLNode: IXMLNode): IXMLNodeList; virtual;
+    function RootNodeCallback: WideString; virtual;
+    function FindNodeCallback(const XMLNode: IXMLNode; const RootNode: WideString): IXMLNodeList; virtual;
   public
     function Decompose(const Item: T): WideString;
     function Compose(const Text: WideString): T;
@@ -60,8 +70,9 @@ type
     function ListDecompose(const List: IIterableList<T>): WideString;
     function ListCompose(const Text: WideString; const List: IIterableList<T>): Boolean;
     function ListComposeFromStream(const Stream: TStream; const List: IIterableList<T>): Boolean;
-    constructor Create(const ItemSerialize: IItemSerialize<T>);
-    class function New(const ItemSerialize: IItemSerialize<T>): ISerialization<T>;
+    constructor Create(const ItemSerialize: IItemSerialize<T>; const ListSerialize: IListSerialize<T> = nil);
+    class function New(const ItemSerialize: IItemSerialize<T>; const ListSerialize: IListSerialize<T> = nil)
+      : ISerialization<T>;
   end;
 
 implementation
@@ -96,32 +107,57 @@ end;
 
 { TXMLListSerialize<T> }
 
+function TXMLListSerialize<T>.AddTagDelimiter(const TagName: WideString; const IsClosed: Boolean): WideString;
+begin
+  Result := '<';
+  if IsClosed then
+    Result := Result + '/';
+  Result := Result + TagName + '>';
+end;
+
 function TXMLListSerialize<T>.Decompose(const List: IIterableList<T>): WideString;
 var
   Item: T;
 begin
-  Result := EmptyStr;
-// TODO:
-// for Item in List do
-// Result := Result + _ItemSerialize.Decompose(Item) + ',';
-// if RightStr(Result, 1) = ',' then
-// Result := Copy(Result, 1, Pred(Length(Result)));
-// Result := '[' + Result + ']';
+  Result := AddTagDelimiter(_RootNodeCallback, False);
+  for Item in List do
+    Result := Result + _ItemSerialize.Decompose(Item);
+  Result := Result + AddTagDelimiter(_RootNodeCallback, True);
 end;
 
 function TXMLListSerialize<T>.Compose(const Text: WideString; const List: IIterableList<T>): Boolean;
+var
+  XMLDoc: IXMLDocument;
+  MainNode: IXMLNode;
+  XMLArray: IXMLNodeList;
+  ArrayElement: IXMLNode;
+  i: NativeInt;
 begin
-// TODO:
+  Result := False;
+  List.Clear;
+  XMLDoc := LoadXMLData(Text);
+  MainNode := XMLDoc.DocumentElement;
+  XMLArray := _FindNodeCallback(MainNode, _RootNodeCallback);
+  for i := 0 to Pred(XMLArray.Count) do
+  begin
+    ArrayElement := XMLArray[i];
+    List.Add(_ItemSerialize.Compose(ArrayElement.XML));
+  end;
+  Result := List.Count > 0;
 end;
 
-constructor TXMLListSerialize<T>.Create(const ItemSerialize: IItemSerialize<T>);
+constructor TXMLListSerialize<T>.Create(const ItemSerialize: IItemSerialize<T>;
+  const FindNodeCallback: TFindNodeCallback; const RootNodeCallback: TRootNodeCallback);
 begin
   _ItemSerialize := ItemSerialize;
+  _FindNodeCallback := FindNodeCallback;
+  _RootNodeCallback := RootNodeCallback;
 end;
 
-class function TXMLListSerialize<T>.New(const ItemSerialize: IItemSerialize<T>): IListSerialize<T>;
+class function TXMLListSerialize<T>.New(const ItemSerialize: IItemSerialize<T>;
+  const FindNodeCallback: TFindNodeCallback; const RootNodeCallback: TRootNodeCallback): IListSerialize<T>;
 begin
-  Result := TXMLListSerialize<T>.Create(ItemSerialize);
+  Result := TXMLListSerialize<T>.Create(ItemSerialize, FindNodeCallback, RootNodeCallback);
 end;
 
 { TXMLSerialization<T> }
@@ -136,30 +172,9 @@ begin
   Result := _ItemSerialize.Compose(Text);
 end;
 
-function TXMLSerialization<T>.FindNodeCallback(const XMLNode: IXMLNode): IXMLNodeList;
-begin
-  Result := XMLNode.ChildNodes;
-end;
-
 function TXMLSerialization<T>.ListCompose(const Text: WideString; const List: IIterableList<T>): Boolean;
-var
-  XMLDoc: IXMLDocument;
-  MainNode: IXMLNode;
-  XMLArray: IXMLNodeList;
-  ArrayElement: IXMLNode;
-  i: NativeInt;
 begin
-  Result := False;
-  List.Clear;
-  XMLDoc := LoadXMLData(Text);
-  MainNode := XMLDoc.DocumentElement;
-  XMLArray := FindNodeCallback(MainNode);
-  for i := 0 to Pred(XMLArray.Count) do
-  begin
-    ArrayElement := XMLArray[i];
-    List.Add(Compose(ArrayElement.XML));
-  end;
-  Result := List.Count > 0;
+  Result := _ListSerialize.Compose(Text, List);
 end;
 
 function TXMLSerialization<T>.ListComposeFromStream(const Stream: TStream; const List: IIterableList<T>): Boolean;
@@ -177,15 +192,29 @@ begin
   Result := Compose(TPlainStream.New.Decode(Stream));
 end;
 
-constructor TXMLSerialization<T>.Create(const ItemSerialize: IItemSerialize<T>);
+function TXMLSerialization<T>.RootNodeCallback: WideString;
 begin
-  _ItemSerialize := TXMLItemSerialize<T>.New(ItemSerialize);
-  _ListSerialize := TXMLListSerialize<T>.New(_ItemSerialize);
+  Result := EmptyWideStr;
 end;
 
-class function TXMLSerialization<T>.New(const ItemSerialize: IItemSerialize<T>): ISerialization<T>;
+function TXMLSerialization<T>.FindNodeCallback(const XMLNode: IXMLNode; const RootNode: WideString): IXMLNodeList;
 begin
-  Result := TXMLSerialization<T>.Create(ItemSerialize);
+  Result := XMLNode.ChildNodes;
+end;
+
+constructor TXMLSerialization<T>.Create(const ItemSerialize: IItemSerialize<T>; const ListSerialize: IListSerialize<T>);
+begin
+  _ItemSerialize := TXMLItemSerialize<T>.New(ItemSerialize);
+  if Assigned(ListSerialize) then
+    _ListSerialize := ListSerialize
+  else
+    _ListSerialize := TXMLListSerialize<T>.New(ItemSerialize, FindNodeCallback, RootNodeCallback);
+end;
+
+class function TXMLSerialization<T>.New(const ItemSerialize: IItemSerialize<T>; const ListSerialize: IListSerialize<T>)
+  : ISerialization<T>;
+begin
+  Result := TXMLSerialization<T>.Create(ItemSerialize, ListSerialize);
 end;
 
 end.
